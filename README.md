b5d6ff318c8dcf5c519e995556e7232e5ffe51df
anuragpatle <anurag.ap6@gmail.com>
2022-09-23 10:13:45 +0530

    adjusted css and docker

--- README.md
+++ README.md
@@ -25,3 +25,10 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## To dockerize this application
+1. Build docker image:
+docker build -t empsentimentmonitoringangular:latest .
+
+2. Run the container
+docker run -d -p 80:80 empsentimentmonitoringangular:latest
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDividerModule } from '@angular/material/divider'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { GoogleChartsModule } from 'angular-google-charts';
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +20,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatDividerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    GoogleChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
